8f5ff326b42219b608d799e5851c9d9cf6a61bd1
Nikolay Petrov <nikolaypetroff777@yandex.ru>
2023-08-08 16:53:33 +0300

    More fixed

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.db.models import Count, OuterRef
 
 from recipes.models import (Favorite, Follow, Ingredient, IngredientInRecipe,
                             Recipe, ShoppingList, Tag)
